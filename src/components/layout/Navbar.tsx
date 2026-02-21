59efe503d8a39d62503034117a593a840553034d
Yubby Dubby <test@example.com>
2026-02-22 00:04:30 +0530

    Adjust header logo spacing to prevent top overlap on desktop

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -20,7 +20,7 @@ export default function Navbar() {
                 </div>
 
                 {/* Center Logo */}
-                <div className="absolute left-1/2 -translate-x-1/2 flex items-center h-full max-h-24 pt-1 md:pt-4">
+                <div className="absolute left-1/2 -translate-x-1/2 flex items-center h-full max-h-24 pt-1 md:pt-1">
                     <Link href="/" className="relative h-[85px] w-[250px] sm:h-[100px] sm:w-[320px] md:h-36 md:w-[480px] flex items-center justify-center">
                         <Image
                             src="/LOGOO.png"
