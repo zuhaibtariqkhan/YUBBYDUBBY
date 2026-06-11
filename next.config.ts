34db9284c9bfd4ba4eae77acf61144426ef5470c
Yubby Dubby <test@example.com>
2026-06-11 20:38:37 +0530

    config: configure next/image to allow remote patterns from shop.yubbydubby.com

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'shop.yubbydubby.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
